8937702a054e2994701367269403f5ec468252aa
Joseph Poncy <jponcy@innovateteam.com>
2016-09-29 15:48:01 -0700

    Customize Constraints EditorPage
    Create dropdown for selection of system of records URL
    Create dropdown for use limitations
    Replicate the Database Editor in EME 4.x using WPF

---
+++ EMEToolKit/Pages/MD_LegalConstraintsSOR.xaml.cs
@@ -0,0 +1,100 @@
+﻿/*
+COPYRIGHT 1995-2009 ESRI
+TRADE SECRETS: ESRI PROPRIETARY AND CONFIDENTIAL
+Unpublished material - all rights reserved under the 
+Copyright Laws of the United States.
+For additional information, contact:
+Environmental Systems Research Institute, Inc.
+Attn: Contracts Dept
+380 New York Street
+Redlands, California, USA 92373
+email: contracts@esri.com
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using ESRI.ArcGIS.Metadata.Editor;
+using ESRI.ArcGIS.Metadata.Editor.Pages;
+namespace EPAMetadataEditor.Pages
+{
+    /// <summary>
+    /// Interaction logic for MD_LegalConstraintsSOR.xaml
+    /// </summary>
+    public partial class MD_LegalConstraintsSOR : EditorPage
+    {
+        public MD_LegalConstraintsSOR()
+        {
+            InitializeComponent();
+        }
+
+        public List<Control> AllChildren(DependencyObject parent)
+        {
+            var _List = new List<Control> { };
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                var _Child = VisualTreeHelper.GetChild(parent, i);
+                if (_Child is Control)
+                    _List.Add(_Child as Control);
+                _List.AddRange(AllChildren(_Child));
+            }
+            return _List;
+        }
+
+        private void cboSystemNames_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //ListBox liBox = (ListBox)lbxLegalConstsOtherLimits;
+            //foreach (var liBoxItem in liBox.Items)
+            //{
+            //    var liBoxCont = liBox.ItemContainerGenerator.ContainerFromItem(liBoxItem);
+            //    var liBoxChildren = AllChildren(liBoxCont);
+            //    string liBoxName = "tbxLegalConstsSystemURL";
+            //    TextBox tbxLegalURL = (TextBox)liBoxChildren.First(c => c.Name == liBoxName);
+            //    tbxLegalURL.Focus();
+            //}
+        }
+
+        private void cboSystemNames_LostFocus(object sender, RoutedEventArgs e)
+        {
+            //ListBox liBox = (ListBox)lbxLegalConstsOtherLimits;
+            //foreach (var liBoxItem in liBox.Items)
+            //{
+            //    var liBoxCont = liBox.ItemContainerGenerator.ContainerFromItem(liBoxItem);
+            //    var liBoxChildren = AllChildren(liBoxCont);
+            //    string liBoxName = "tbxLegalConstsSystemURL";
+            //    TextBox tbxLegalURL = (TextBox)liBoxChildren.First(c => c.Name == liBoxName);
+            //    string liBoxName2 = "cboSystemNames";
+            //    ComboBox cboSysURL = (ComboBox)liBoxChildren.First(c => c.Name == liBoxName2);
+            //    tbxLegalURL.Text = cboSysURL.Text;
+            //    tbxLegalURL.Focus();
+            //}
+        }
+
+        private void cboSystemNames_LostMouseCapture(object sender, MouseEventArgs e)
+        {
+            ListBox liBox = (ListBox)lbxLegalConstsOtherLimits;
+            foreach (var liBoxItem in liBox.Items)
+            {
+                var liBoxCont = liBox.ItemContainerGenerator.ContainerFromItem(liBoxItem);
+                var liBoxChildren = AllChildren(liBoxCont);
+                string liBoxName = "tbxLegalConstsSystemURL";
+                TextBox tbxLegalURL = (TextBox)liBoxChildren.First(c => c.Name == liBoxName);
+                string liBoxName2 = "cboSystemNames";
+                ComboBox cboSysURL = (ComboBox)liBoxChildren.First(c => c.Name == liBoxName2);
+                tbxLegalURL.Text = cboSysURL.Text;
+                tbxLegalURL.Focus();
+            }
+        }
+    }
+}
---
+++ EMEdbManager/EMEdbManager/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EMEdbManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string appPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+            XmlRecord_ThemeKeyEpa.Source = new Uri("C:\\Users\\Innovate\\AppData\\Roaming\\Innovate! Inc\\EPA Metadata Edtior 4x\\Eme4xSystemFiles\\EMEdb\\KeywordsEPA.xml");
+            //XmlRecord_ThemeKeyUser.Source = new Uri("C:\\Users\\Innovate\\AppData\\Roaming\\Innovate! Inc\\EPA Metadata Edtior 4x\\Eme4xSystemFiles\\EMEdb\\KeywordsUser.xml");
+            //XmlRecord_PlaceKey.Source = new Uri("C:\\Users\\Innovate\\AppData\\Roaming\\Innovate! Inc\\EPA Metadata Edtior 4x\\Eme4xSystemFiles\\EMEdb\\KeywordsPlace.xml");
+            XmlRecord_SystemofRecords.Source = new Uri("C:\\Users\\Innovate\\AppData\\Roaming\\Innovate! Inc\\EPA Metadata Edtior 4x\\Eme4xSystemFiles\\EMEdb\\SystemofRecords.xml");
+            XmlRecord_SecConsts.Source = new Uri("C:\\Users\\Innovate\\AppData\\Roaming\\Innovate! Inc\\EPA Metadata Edtior 4x\\Eme4xSystemFiles\\EMEdb\\SecurityConstraints.xml");
+            // C:\Users\Innovate\AppData\Roaming\Innovate! Inc\EPA Metadata Edtior 4x\Eme4xSystemFiles\EMEdb\
+            //labelXMLPath.Content = KeywordData.Source.LocalPath;
+        }
+
+        private void lbxThemekeyEpa_loaded(object sender, RoutedEventArgs e)
+        {
+            lblKwEPACount.Content = lbxThemekeyEpa.Items.Count.ToString();
+        }
+
+        private void lbxSystemofRecords_loaded(object sender, RoutedEventArgs e)
+        {
+            lblSoRCount.Content = lbxSystemofRecords.Items.Count.ToString();
+        }
+
+        private void lbxSecConsts_loaded(object sender, RoutedEventArgs e)
+        {
+            lblSecConstsCount.Content = lbxSecConsts.Items.Count.ToString();
+        }
+
+        private void btnSaveThemekeyEpa_Click(object sender, RoutedEventArgs e)
+        {
+            string source = XmlRecord_ThemeKeyEpa.Source.LocalPath;
+            XmlRecord_ThemeKeyEpa.Document.Save(source);
+        }
+
+        private void btnSaveSOR_Click(object sender, RoutedEventArgs e)
+        {
+            string source = XmlRecord_SystemofRecords.Source.LocalPath;
+            XmlRecord_SystemofRecords.Document.Save(source);
+        }
+
+        private void btnSaveSecConsts_Click(object sender, RoutedEventArgs e)
+        {
+            string source = XmlRecord_SecConsts.Source.LocalPath;
+            XmlRecord_SecConsts.Document.Save(source);
+        }
+    }
+}
